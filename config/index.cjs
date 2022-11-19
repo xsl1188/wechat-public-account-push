34510f113b84a11aabd4fd295ef60275570d1707
xsl1188 <xsl1188@users.noreply.github.com>
2022-11-20 00:18:01 +0800

    Create index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oDQtk6uBhVLx0CrK_475swWlclYI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'VcjNMHtyhCCSMD1wpfbQcAucikgnTiSvQufP93MXm_k',
+      useTemplateId: 'iFIpOqDUFYKPQLRdN4IW0LcxCdSva3OWHJ2vORThbBw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-27',
       festivals: [
